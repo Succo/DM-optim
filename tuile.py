176eb2617b53650e6e6b828aec0e47b198c3499c
Fabrice <fabrice.vaillant@centraliens.net>
2017-01-04 11:26:15 +0100

    Adds context management to the solver

--- tuile.py
+++ tuile.py
@@ -19,6 +19,7 @@ class Tuile():
     def __init__(self, val):
         val = int(val, 16)
         self.possible = getPossible(val)
+        self.assigned = None
 
     def connectUp(self, conn):
         """ connectUp imposes or not a path Up based on conn value """
@@ -44,9 +45,14 @@ class Tuile():
 class Grille():
     """ grille is the main class, has the array of tuile """
     def __init__(self, width, height, tuiles):
+        # Dimension of the grid
         self.width = width
         self.height = height
         self.tuiles = tuiles
+        # Number of nodes in the tree
+        self.nodes = 0
+        # Context, i.e. list of effectued modification
+        self.context = []
 
     def get(self, x, y):
         return self.tuiles[y*self.width+x]
@@ -68,3 +74,42 @@ class Grille():
     def prettyPrint(self):
         for i, t in enumerate(self.tuiles):
             print(t.possible)
+
+    def solve(self):
+        """ iterator on all solutions """
+        self.nodes += 1
+        t = self.selectVar()
+        if t is None:
+            yield [t.assigned for t in self.tuiles]
+        else:
+            for orient in t.possible:
+                t.assigned = orient
+                history = self.save_context()
+                self.restore_context(history)
+
+    # context management
+    def save_context(self):
+        return len(self.context)
+
+    def restore_context(self, history):
+        """ restore tuile to their previous values """
+        while len(self.context) > history:
+            i, tuile = self.context.pop()
+            self.tuiles[i] = tuile
+
+    def update_tuile(self, i, tuile):
+        """ update a tuile storing it's previous value in context """
+        self.context.append(i, self.tuiles[i])
+        self.tuiles[i] = tuile
+
+    # exploration
+    def selectVar(self):
+        """ returns the next tuile on which to iterate
+        heuristic: the one with the smallest domain """
+        choice = None
+        for t in self.tuiles:
+            if t.assigned is None and  \
+                (choice is None or
+                    len(t.possible) < len(choice.possible)):
+                choice = t
+        return choice
