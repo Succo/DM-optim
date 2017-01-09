6b0213926a0ff1d8c1f44613684ef3d4eb05ec00
Fabrice <fabrice.vaillant@centraliens.net>
2017-01-09 17:20:26 +0100

    Adds a generator and a -g option

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
+import random
 import sys
 from tuile import Grille, Tuile
 
 
-def main():
-    args = sys.argv
-    if len(args) != 2 or args[1] != "-s":
-        print("Run the program with -s and input from stdin")
-        return
+def grid_from_stdin():
     height = 0
     width = 0
     tuiles = []
@@ -18,7 +15,42 @@ def main():
         for i in range(width):
             tuiles += [Tuile(line[i], i, height)]
         height += 1
-    g = Grille(width, height, tuiles)
+    return Grille(width, height, tuiles)
+
+
+def generate_grid(args):
+    """ generates a grid of random tuiles """
+    values = [format(i, 'x') for i in range(16)]
+    side_values = [format(i, 'x') for i in range(15)]
+    corner_values = [format(i, 'x') for i in range(12)]
+    height = int(args[0])
+    if len(args) > 1 and args[1].isdigit():
+        width = int(args[1])
+    else:
+        width = height
+    tuiles = []
+    for i in range(height):
+        for j in range(width):
+            if (i == 0 or i == height-1) and\
+               (j == 0 or j == width-1):
+                tuiles += [Tuile(random.choice(corner_values), j, i)]
+            elif (i == 0 or i == height-1) or\
+                 (j == 0 or j == width-1):
+                tuiles += [Tuile(random.choice(side_values), j, i)]
+            else:
+                tuiles += [Tuile(random.choice(values), j, i)]
+    return Grille(width, height, tuiles)
+
+
+def main():
+    args = sys.argv
+    if len(args) < 2 or args[1] not in ["-s", "-g", "-h"]:
+        print("Run the program with -s to read from stdin or -g and size")
+        return
+    if args[1] == "-s":
+        g = grid_from_stdin()
+    else:
+        g = generate_grid(args[2:])
     g.constrain_border()
     g.maintain_arc_consistency()
     for sol in g.solve():
